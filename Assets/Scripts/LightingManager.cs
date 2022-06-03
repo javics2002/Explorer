b734e59f75277f6bebeb24b9cfe54d80e2ffb884
javics2002 <49459590+javics2002@users.noreply.github.com>
2022-06-03 11:30:31 +0200

    Estrellas :star:

--- Assets/Scripts/LightingManager.cs
+++ Assets/Scripts/LightingManager.cs
@@ -17,10 +17,14 @@ public class LightingManager : MonoBehaviour
     [SerializeField, Range(0, 1)] float virtualTime;
 
     DateTime time;
+    ParticleSystem stars;
+    ParticleSystem.Particle[] particles;
 
     private void Start()
     {
         time = DateTime.Now;
+        stars = GetComponent<ParticleSystem>();
+        particles = new ParticleSystem.Particle[10000];
     }
 
     private void Update()
@@ -37,7 +41,7 @@ public class LightingManager : MonoBehaviour
     {
         //Porcentaje del día, siendo 0 las 0:00:00 y 0,5 las 12:00:00
         float timePercent = useVirtualTime ? virtualTime : time.Hour / 24.0f + time.Minute / (24.0f * 60) + time.Second / (24.0f * 3600);
-        Debug.Log("Porcentaje del dia: " + timePercent);
+        //Debug.Log("Porcentaje del dia: " + timePercent);
 
         //Set ambient and fog
         RenderSettings.ambientLight = preset.AmbientColor.Evaluate(timePercent);
@@ -50,8 +54,20 @@ public class LightingManager : MonoBehaviour
             //Las 15:00:00 son la hora en el que el sol está más alto, y sunHeight es 1.
             //-1 es la mitad de la noche y 0 el amanecer y atardecer.
             float sunHeight = Mathf.Sin(2 * Mathf.PI * timePercent - 2.36f);
-            Debug.Log("Altura del sol: " + sunHeight);
+            //Debug.Log("Altura del sol: " + sunHeight);
             sun.transform.localRotation = Quaternion.Euler(new Vector3(sunHeight * (90.0f - angle), timePercent * 360f - sunOffset, 0));
+
+            //Las estrellas solo se ven de noche. En el amanecer y atardecer, aparecen y desaparecen poco a poco.
+            byte alpha = (byte) (sunHeight < 0.0f ? 128 : sunHeight > 0.2f ? 0 : 128 - sunHeight * 640);
+
+            //Cambiamos el alpha de cada particle
+            stars.GetParticles(particles);
+            for (int i = 0; i < particles.Length; i++) {
+                Color32 color = particles[i].startColor;
+                color.a = alpha;
+                particles[i].startColor = color;
+            }
+            stars.SetParticles(particles);
         }
     }
 
